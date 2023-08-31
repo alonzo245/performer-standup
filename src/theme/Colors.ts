51d475b8b4f112ca934680a57c660e2b4d0f60de
Alon <alon@Alons-MacBook-Pro-2.local>
2023-08-31 11:39:07 +0300

    save

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,6 +1,8 @@
-export default {
+const COLORS =  {
     white: '#ffffff',
     black: '#000000',
     purpleTheme: { first: '#17212b', secondery: '#3F0071', third: '#150050', fourth: '#090024' },
     peachTheme: { first: '#F39189', secondery: '#BB8082', third: '#6E7582', fourth: '#046582' },
 };
+
+export default COLORS;
